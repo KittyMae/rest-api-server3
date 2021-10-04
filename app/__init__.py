493910025ff74889d3e7c899a30aa096f45ca974
Bara D <KittyMae@seznam.cz>
2021-10-04 04:10:45 +0200

    Fix runtime issues

--- app/__init__.py
+++ app/__init__.py
@@ -14,25 +14,22 @@ def create_app(test_config=None):
     else:
         # load the test config if passed in
         app.config.update(test_config)
-    
-    if not os.path.isdir(app.config['UPLOAD_FOLDER']):
-        os.mkdir(app.config['UPLOAD_FOLDER'])
-    
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile("config.py", silent=True)
-    else:
-        # load the test config if passed in
-        app.config.update(test_config)
-
+        
     # ensure the instance folder exists
     try:
         os.makedirs(app.instance_path)
     except OSError:
         pass
 
+    if not os.path.isdir(app.config['UPLOAD_FOLDER']):
+        os.mkdir(app.config['UPLOAD_FOLDER'])
+
     @app.route("/hello")
     def hello():
         return "Hello, World!"
+
+    # register API blueprint
+    from app.api import api
+    app.register_blueprint(api, url_prefix="/api/v1")
 
     return app
